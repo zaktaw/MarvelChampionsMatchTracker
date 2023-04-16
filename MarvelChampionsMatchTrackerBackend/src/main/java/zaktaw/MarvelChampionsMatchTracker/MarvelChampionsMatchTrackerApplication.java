27332180fc6fca194142ba7bd50d2b05f97e8209
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2023-04-16 10:25:59 +0200

    Add Spring Boot project

---
+++ MarvelChampionsMatchTrackerBackend/src/main/java/zaktaw/MarvelChampionsMatchTracker/MarvelChampionsMatchTrackerApplication.java
@@ -0,0 +1,13 @@
+package zaktaw.MarvelChampionsMatchTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelChampionsMatchTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelChampionsMatchTrackerApplication.class, args);
+	}
+
+}
---
+++ MarvelChampionsMatchTrackerBackend/src/test/java/zaktaw/MarvelChampionsMatchTracker/MarvelChampionsMatchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package zaktaw.MarvelChampionsMatchTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChampionsMatchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
