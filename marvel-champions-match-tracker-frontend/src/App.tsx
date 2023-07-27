4b3555ebfc55d0ffd16d66f39adccc46dba8c27f
Zakaria Karlsen Tawfiq <zaktaw@gmail.com>
2023-07-27 19:12:50 +0200

    Get data from match API

--- marvel-champions-match-tracker-frontend/src/App.tsx
+++ marvel-champions-match-tracker-frontend/src/App.tsx
@@ -1,10 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
+interface Match {
+  id: number;
+  hero: string;
+  deck: string;
+  villain: string;
+  expertMode: boolean;
+  modules: string[];
+  won: boolean;
+  timestamp: Date;
+}
+
 function App() {
+
+  const [matches, setMatches] = useState<Match[] | null>(null);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/match')
+      .then(data => {
+        data.json().then((json: Match[]) => {
+          setMatches(json);
+        })
+      });
+  }, [])
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Matches</h1>
+      {matches?.map(match => <p>- {match.timestamp.toString()}: {match.hero} vs {match.villain}: {match.won ? "WIN" : "LOSS"}</p>)}
     </div>
   );
 }
